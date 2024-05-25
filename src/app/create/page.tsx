1c947b0e90c6cda9f2065c15d471326e91dab8a8
Soham Pagi <paguisoham36@gmail.com>
2024-05-25 13:41:52 +0530

    Add kinde auth, init prisma sqlite

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,38 @@
+import { createPost } from "@/actions/actions";
+import LogoutLink from "@/components/LogoutLink";
 import React from "react";
 
 export default function CreatePage() {
-  return <div>page</div>;
+  return (
+    <main className="text-center pt-16">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
+
+      <form
+        action={createPost}
+        className="flex flex-col max-w-[400px] mx-auto gap-2 my-10"
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Title for new post"
+          className="border rounded px-3 h-10"
+          required
+        />
+
+        <textarea
+          name="body"
+          placeholder="Body content for new post"
+          className="border rounded px-3 py-2"
+          rows={6}
+          required
+        />
+
+        <button className="h-10 bg-blue-500 px-5 rounded text-white">
+          Submit
+        </button>
+      </form>
+
+      <LogoutLink>Log out</LogoutLink>
+    </main>
+  );
 }
---
+++ src/components/LogoutLink.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+
+export default LogoutLink;
